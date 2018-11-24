7aa110091169982092f86e4f3d4be41fb67117a7
Bartłomiej Hołota <bartekholota@gmail.com>
2018-11-24 21:48:36 +0100

    Response for cd status - playing by default

--- ibus/src/main/java/com/bholota/ibus/module/CDPlayerModule.kt
+++ ibus/src/main/java/com/bholota/ibus/module/CDPlayerModule.kt
@@ -11,15 +11,26 @@ import com.bholota.ibus.frame.IBusFrame
  */
 class CDPlayerModule : IBusModule() {
 
-    private val midToCdPooling = IBusFrame(IBusDevice.Radio, IBusDevice.CDPlayer, listOf(0x1))
-
+    private val midToCdPoolingRequest = IBusFrame(IBusDevice.Radio, IBusDevice.CDPlayer, listOf(0x1))
     private val cdToMidPoolingResponse = IBusFrame(IBusDevice.CDPlayer, IBusDevice.Broadcast2, listOf(0x2, 0x0))
+
+    //private val midToCdStatusRequest = IBusFrame(IBusDevice.Radio, IBusDevice.CDPlayer, listOf(0x0, 0x0)) // 0x38, 0x0, 0x0
+    //private val cdToMidPlayingResponse = IBusFrame(IBusDevice.Radio, IBusDevice.CDPlayer, listOf(0x1))    // 0x38, 0x3, 0x0
+//    if playing {
+//        message = []byte{0x39, 0x00, 0x09, 0x00, 0x3f, 0x00}
+//    } else {
+//        message = []byte{0x39, 0x00, 0x02, 0x00, 0x3f, 0x00}
+//    }
+
+    private val midToCdStatusRequest = IBusFrame(IBusDevice.Radio, IBusDevice.CDPlayer, listOf(0x38, 0x0, 0x0))
+    private val cdToMidPlayingResponse = IBusFrame(IBusDevice.CDPlayer, IBusDevice.Radio, listOf(0x39, 0x0, 0x9, 0x0, 0x3f, 0x0))
 
     override fun onRequest(connection: UartConnection, frame: IBusFrame) {
         L.log("CDPlayerModule --> onRequest: $frame")
 
         when(frame) {
-            midToCdPooling -> connection.writeData(cdToMidPoolingResponse.toByteArray())
+            midToCdPoolingRequest -> connection.writeData(cdToMidPoolingResponse.toByteArray())
+            midToCdStatusRequest -> connection.writeData(cdToMidPlayingResponse.toByteArray())
         }
     }
 
