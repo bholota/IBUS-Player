ae7be36aeb8f706fe7771495dc15802c3971ebfa
Bartłomiej Hołota <bartekholota@gmail.com>
2018-11-25 00:39:02 +0100

    Fix missing cd number

--- ibus/src/main/java/com/bholota/ibus/module/CDPlayerModule.kt
+++ ibus/src/main/java/com/bholota/ibus/module/CDPlayerModule.kt
@@ -6,31 +6,24 @@ import com.bholota.ibus.UartConnection
 import com.bholota.ibus.frame.IBusFrame
 
 /**
- * 1. every 30s announce until pool
+ * 1. every 30s announce until first pool listOf(0x2, 0x1)
  * 2. if pool msg received send pool response, go to 2.
  */
 class CDPlayerModule : IBusModule() {
 
-    private val midToCdPoolingRequest = IBusFrame(IBusDevice.Radio, IBusDevice.CDPlayer, listOf(0x1))
-    private val cdToMidPoolingResponse = IBusFrame(IBusDevice.CDPlayer, IBusDevice.Broadcast2, listOf(0x2, 0x0))
-
-    //private val midToCdStatusRequest = IBusFrame(IBusDevice.Radio, IBusDevice.CDPlayer, listOf(0x0, 0x0)) // 0x38, 0x0, 0x0
-    //private val cdToMidPlayingResponse = IBusFrame(IBusDevice.Radio, IBusDevice.CDPlayer, listOf(0x1))    // 0x38, 0x3, 0x0
-//    if playing {
-//        message = []byte{0x39, 0x00, 0x09, 0x00, 0x3f, 0x00}
-//    } else {
-//        message = []byte{0x39, 0x00, 0x02, 0x00, 0x3f, 0x00}
-//    }
-
-    private val midToCdStatusRequest = IBusFrame(IBusDevice.Radio, IBusDevice.CDPlayer, listOf(0x38, 0x0, 0x0))
-    private val cdToMidPlayingResponse = IBusFrame(IBusDevice.CDPlayer, IBusDevice.Radio, listOf(0x39, 0x0, 0x9, 0x0, 0x3f, 0x0))
+    private val cdAnnounce = IBusFrame(IBusDevice.CDPlayer, IBusDevice.Broadcast2, listOf(0x2, 0x1)) // todo trigger every 30s until first pooling
+    private val cdPoolingRequest = IBusFrame(IBusDevice.Radio, IBusDevice.CDPlayer, listOf(0x1))
+    private val cdPoolingResponse = IBusFrame(IBusDevice.CDPlayer, IBusDevice.Broadcast2, listOf(0x2, 0x0))
+    private val cdStatusRequest = IBusFrame(IBusDevice.Radio, IBusDevice.CDPlayer, listOf(0x38, 0x0, 0x0))
+    private val cdPlayingResponse = IBusFrame(IBusDevice.CDPlayer, IBusDevice.Radio, listOf(0x39, 0x0, 0x9, 0x0, 0x3f, 0x0, /*disk index 1-6*/0x1))
+    private val cdStoppedResponse = IBusFrame(IBusDevice.CDPlayer, IBusDevice.Radio, listOf(0x39, 0x0, 0x2, 0x0, 0x3f, 0x0, /*disk index 1-6*/0x1))
 
     override fun onRequest(connection: UartConnection, frame: IBusFrame) {
         L.log("CDPlayerModule --> onRequest: $frame")
 
         when(frame) {
-            midToCdPoolingRequest -> connection.writeData(cdToMidPoolingResponse.toByteArray())
-            midToCdStatusRequest -> connection.writeData(cdToMidPlayingResponse.toByteArray())
+            cdPoolingRequest -> connection.writeData(cdPoolingResponse.toByteArray())
+            cdStatusRequest -> connection.writeData(cdPlayingResponse.toByteArray())
         }
     }
 
